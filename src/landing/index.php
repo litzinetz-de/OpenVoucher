ef566e5dd69868e8b1d250f3424f7dd52f638fe8
litzi0815 <danny@litzinetz.de>
2013-10-28 09:01:03 +0100

    Added authentication form

--- src/landing/index.php
+++ src/landing/index.php
@@ -3,4 +3,17 @@ include('../includes/header.php');
 require('../classes/vouchermanager.php');
 
 $v = new vouchermanager();
+
+if($v->ClientAuthenticated())
+{
+	echo 'You are online.';
+} else {
+	echo 'Please add your voucher code in the form below to get internet access.<br><br>
+	<form action="auth.php" method="post">
+	Voucher code: <input type="text" name="vid" size="20" class="formstyle"><br><br>
+	<input type="submit" value="OK" class="formstyle">
+	</form>';
+}
 ?>
+</body>
+</html>

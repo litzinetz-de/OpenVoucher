c9b92aaacfa66217cdb777a086b3a36cdedb2892
litzi0815 <danny@litzinetz.de>
2013-10-31 10:24:10 +0100

    PDF-Header

--- src/classes/printvoucher.php
+++ src/classes/printvoucher.php
@@ -1,6 +1,55 @@
 <?php
+require('fpdf/fpdf.php');
+
+class PDF extends FPDF
+{
+	// Page header
+	function Header()
+	{
+		// Logo
+		$this->Image('../graphics/logo-small.png',10,6,15);
+		// Arial bold 15
+		$this->SetFont('Arial','B',15);
+		// Move to the right
+		$this->Cell(80);
+		// Title
+		$this->Cell(30,10,'OpenVoucher',0,0,'C');
+		// Line
+		$this->Line(0,25,220,25);
+	}
+
+	// Page footer
+	function Footer()
+	{
+		// Position at 1.5 cm from bottom
+		$this->SetY(-15);
+		// Arial italic 8
+		$this->SetFont('Arial','I',8);
+		// Page number
+		$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+}
+
 class printvoucher
 {
-	//
+	private $pdf;
+	private $vouchertype;
+	private $voucherlist;
+	
+	// vouchertype: small (many vouchers per page) or big (one voucher per page)
+	// voucherlist: array of voucher IDs to print
+	function __construct($vouchertype,$voucherlist)
+	{
+		$this->pdf=new PDF();
+		$this->vouchertype=$vouchertype;
+		$this->voucherlist=$voucherlist;
+		
+		$this->pdf = new PDF();
+		$this->pdf->AliasNbPages();
+		$this->pdf->AddPage();
+		$this->pdf->SetFont('Arial','',12);
+		
+		$this->pdf->Output();
+	}
 }
 ?>

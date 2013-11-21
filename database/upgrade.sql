0f1d718dcbce0e2e0387ac655e9d91ced347483a
litzi0815 <danny@litzinetz.de>
2013-11-21 10:41:02 +0100

    Modified SQL query for DB upgrade

--- database/upgrade.sql
+++ database/upgrade.sql
@@ -1,2 +1,4 @@
 USE openvoucher;
-CREATE TABLE settings (setting VARCHAR(255) NOT NULL PRIMARY KEY,value VARCHAR(255) NOT NULL);
+CREATE TABLE settings (setting VARCHAR(255) NOT NULL PRIMARY KEY,s_value VARCHAR(255) NOT NULL);
+INSERT INTO settings (setting,s_value) VALUES ('vouchertext1','Please enter the code');
+INSERT INTO settings (setting,s_value) VALUES ('vouchertext2','to get internet access');

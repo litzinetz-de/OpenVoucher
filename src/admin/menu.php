05ade4fadb12f4bfc855c1227074bcd5d4ea0d00
litzi0815 <danny@litzinetz.de>
2013-11-21 10:42:39 +0100

    Updated menu

--- src/admin/menu.php
+++ src/admin/menu.php
@@ -5,7 +5,7 @@ echo '<center><div class="small">Admin interface</div></center>
 
 if($a->CheckPermission('add_voucher')) { echo '<a href="addvoucher.php">Issue voucher(s)</a> - '; }
 if($a->CheckPermission('view_users')) { echo '<a href="users.php">Manage user accounts</a> - '; }
-//if($a->CheckPermission('sys_config')) { echo '<a href="config.php">Configure system</a> - '; } // This will be available in the next version
+if($a->CheckPermission('sys_config')) { echo '<a href="sysconfig.php">Configure system</a> - '; } // This will be available in the next version
 
 echo '<a href="about.php">About / Version</a> - <a href="logout.php">Logout</a>
 </ul><br>';

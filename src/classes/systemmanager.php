0cb9195690d74d61bb63384ecc063571bc729371
litzi0815 <danny@litzinetz.de>
2013-11-29 14:42:08 +0100

    We have to check if systemmanager has already been declared

--- src/classes/systemmanager.php
+++ src/classes/systemmanager.php
@@ -1,48 +1,51 @@
 <?php
 require_once('../includes/config.php');
 
-class systemmanager
+if(!class_exists('systemmanager'))
 {
-	private $mysqlconn;
-	private $defaults; // This array contains the default values of nothing is configured in mysql database
-	
-	function __construct()
+	class systemmanager
 	{
-		$this->defaults['vouchertext1']='Please enter the code';
-		$this->defaults['vouchertext2']='to get internet access';
-		$this->defaults['pre-form-text']='Please add your voucher code in the form below to get internet access.';
-		$this->defaults['post-form-text']='Feel free to contact an administrator if you have any problems.';
-		$this->defaults['use_verification']='n';
+		private $mysqlconn;
+		private $defaults; // This array contains the default values of nothing is configured in mysql database
 		
-		$this->mysqlconn=mysql_connect(MYSQL_HOST,MYSQL_USER,MYSQL_PWD);
-		mysql_select_db(MYSQL_DB,$this->mysqlconn);
-	}
-	
-	public function GetSetting($setting)
-	{
-		$res=mysql_query('SELECT s_value FROM settings WHERE setting="'.$setting.'"',$this->mysqlconn);
-		$row=mysql_fetch_array($res);
-		$return_value=$row['s_value'];
+		function __construct()
+		{
+			$this->defaults['vouchertext1']='Please enter the code';
+			$this->defaults['vouchertext2']='to get internet access';
+			$this->defaults['pre-form-text']='Please add your voucher code in the form below to get internet access.';
+			$this->defaults['post-form-text']='Feel free to contact an administrator if you have any problems.';
+			$this->defaults['use_verification']='n';
+			
+			$this->mysqlconn=mysql_connect(MYSQL_HOST,MYSQL_USER,MYSQL_PWD);
+			mysql_select_db(MYSQL_DB,$this->mysqlconn);
+		}
 		
-		if($return_value=='')
+		public function GetSetting($setting)
 		{
-			return $this->defaults[$setting];
-		} else {
-			return $return_value;
+			$res=mysql_query('SELECT s_value FROM settings WHERE setting="'.$setting.'"',$this->mysqlconn);
+			$row=mysql_fetch_array($res);
+			$return_value=$row['s_value'];
+			
+			if($return_value=='')
+			{
+				return $this->defaults[$setting];
+			} else {
+				return $return_value;
+			}
 		}
-	}
-	
-	public function SetSetting($setting,$value)
-	{
-		$res=mysql_query('SELECT COUNT(*) AS cnt FROM settings WHERE setting="'.$setting.'"',$this->mysqlconn);
-		$row=mysql_fetch_array($res);
-		if($row['cnt']>0)
+		
+		public function SetSetting($setting,$value)
 		{
-			$query='UPDATE settings SET s_value="'.$value.'" WHERE setting="'.$setting.'"';
-		} else {
-			$query='INSERT INTO settings (setting,s_value) VALUES ("'.$setting.'","'.$value.'")';
+			$res=mysql_query('SELECT COUNT(*) AS cnt FROM settings WHERE setting="'.$setting.'"',$this->mysqlconn);
+			$row=mysql_fetch_array($res);
+			if($row['cnt']>0)
+			{
+				$query='UPDATE settings SET s_value="'.$value.'" WHERE setting="'.$setting.'"';
+			} else {
+				$query='INSERT INTO settings (setting,s_value) VALUES ("'.$setting.'","'.$value.'")';
+			}
+			mysql_query($query,$this->mysqlconn);
 		}
-		mysql_query($query,$this->mysqlconn);
 	}
 }
 ?>

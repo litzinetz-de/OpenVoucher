712f749ce2bb33e916791d206457c2e610ca7519
litzi0815 <danny@litzinetz.de>
2013-12-02 10:11:43 +0100

    Included needed classes

--- src/admin/api.php
+++ src/admin/api.php
@@ -5,4 +5,10 @@ require('../classes/systemmanager.php');
 require('../classes/usermanager.php');
 require('../classes/versionmanager.php');
 require('../classes/vouchermanager.php');
+
+$auth = new adminauth('api');
+$systemmanager = new systemmanager();
+$usermanager = new usermanager();
+$versionmanager = new versionmanager();
+$vouchermanager = new vouchermanager();
 ?>

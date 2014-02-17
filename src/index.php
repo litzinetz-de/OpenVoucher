9411f94fef98a2bb198550e7fdbe457e4a434a52
litzi0815 <danny@litzinetz.de>
2014-02-17 10:16:07 +0100

    Changed redirection
    Redirection to landing page will now work on any path on the webserver

--- src/index.php
+++ src/index.php
@@ -1,4 +1,9 @@
 <?php
-header('Location: http://'.$_SERVER['SERVER_ADDR'].'/landing/');
+
+$temp=explode('/',$_SERVER['SCRIPT_NAME']);
+unset($temp[count($temp)-1]);
+$path=join('/',$temp);
+
+header('Location: '.$path.'/landing/');
 die();
 ?>

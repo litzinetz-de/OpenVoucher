653d65341456964023ef50bd106c97eff0cd1e99
litzi0815 <danny@litzinetz.de>
2014-02-17 11:37:48 +0100

    Changed cronjob to call php script directly
    Also added comment to php script

--- src/localscripts/refresh_permissions.php
+++ src/localscripts/refresh_permissions.php
@@ -1,5 +1,7 @@
 <?php
-chdir ('/var/www/localscripts/');
+chdir ('/var/www/localscripts/'); // Edit this path if needed
+
+// Don't edit below!
 include('../classes/vouchermanager.php');
 $v = new vouchermanager();
 $v->DropOldVouchers();

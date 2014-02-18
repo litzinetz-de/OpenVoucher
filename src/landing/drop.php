6c51e20b1a839995593bda6c4c551fb5dca72eb8
litzi0815 <danny@litzinetz.de>
2014-02-18 18:31:07 +0100

    Added more functions to drop.php

--- src/landing/drop.php
+++ src/landing/drop.php
@@ -35,6 +35,39 @@ if($_GET['do']=='lst-devices')
 			die();
 		}
 	}
+	
+	$voucher_info=$v->GetVoucherInfo($_POST['vid']);
+	$devices=$v->GetDeviceList($_POST['vid']);
+	
+	echo 'Device count: '.$voucher_info['dev_count'].' ('.$voucher_info['remain'].' left)<br>
+	Valid until: '.date('Y-m-d H:i',$voucher_info['valid_until']).'<br><br>Choose one of these devices to drop:<br>';
+	
+	foreach($devices as $device)
+	{
+		echo '<ul><a href="'.$_SERVER['PHP_SELF'].'?do=drop&vid='.$_POST['vid'].'&verification_key='.$_POST['verification_key'].'&device='.$device.'">'.$addr.'</a></ul>';
+	}
+}
+
+if($_GET['do']=='drop')
+{
+	if($s->GetSetting('use_verification')=='y')
+	{
+		if(!$v->VerifyVoucherKey($_GET['vid'],$_GET['verification_key']))
+		{
+			echo 'The verification key is invalid. Please go <a href="'.$_SERVER['PHP_SELF'].'">back</a> and try again.</body></html>';
+			die();
+		}
+	}
+	
+	if(!isset($_GET['device']))
+	{
+		echo 'Couldn\'t get device.</body></html>';
+		die();
+	}
+	if(filter_var($_GET['addr'], FILTER_VALIDATE_IP))
+	{
+		//
+	}
 }
 
 ?>
